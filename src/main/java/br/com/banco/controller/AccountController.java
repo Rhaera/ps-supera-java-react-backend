aaffaf7b43ef6d43ef75b54e9151981916b0139f
Rhaera <rauldantas@usp.br>
2023-07-20 20:21:29 -0300

    feat: Estrutura Requerida pelo Projeto Implementada nos Controllers

--- src/main/java/br/com/banco/controller/AccountController.java
+++ src/main/java/br/com/banco/controller/AccountController.java
@@ -1,19 +1,39 @@
 package br.com.banco.controller;
 
+import br.com.banco.model.BankAccount;
+import br.com.banco.model.dto.AccountDto;
 import br.com.banco.service.BankAccountService;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/accounts")
-@CrossOrigin(value = "*")
+@CrossOrigin(value = "**")
 @RequiredArgsConstructor
 public class AccountController {
 
     private final BankAccountService service;
 
+    @PostMapping(value = "/")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Optional<AccountDto> postAccount(@RequestParam(value = "newUser") String name) {
+        return service.createNewAccount(new BankAccount(name));
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Optional<AccountDto> getAccountById(@PathVariable(value = "id") long id) {
+        return service.readAccountById(id);
+    }
+
+    @GetMapping(value = "/{name}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Optional<AccountDto> getAccountByName(@PathVariable(value = "name") String name) {
+        return service.readAccountByName(name);
+    }
 }
--- src/main/java/br/com/banco/controller/TransferController.java
+++ src/main/java/br/com/banco/controller/TransferController.java
@@ -1,19 +1,39 @@
 package br.com.banco.controller;
 
+import br.com.banco.model.TransferEntity;
+import br.com.banco.model.dto.TransferEntityDto;
 import br.com.banco.service.TransferService;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/transfers")
-@CrossOrigin(value = "*")
+@CrossOrigin(value = "**")
 @RequiredArgsConstructor
 public class TransferController {
 
     private final TransferService service;
 
+    @PostMapping(value = "/")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Optional<TransferEntityDto> postTransfer(@RequestBody @Validated(value = TransferEntity.class) TransferEntity newTransfer) {
+        return service.insertTransfer(newTransfer);
+    }
+    @GetMapping(value = "/{accountId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<TransferEntityDto> getAllByAccountId(@PathVariable(value = "accountId") long accountId) {
+        return service.listAllByAccountId(accountId);
+    }
+    @GetMapping(value = "/{originName}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<TransferEntityDto> getAllByOriginName(@PathVariable(value = "originName") String origin) {
+        return service.listAllByOriginName(origin);
+    }
 }
